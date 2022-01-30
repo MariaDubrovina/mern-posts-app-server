720f070d95533ad879dc6d3782edd5ca7c13d08d
Maria Dubrovina <mari.dubrovina@gmail.com>
2022-01-30 21:02:24 +0500

    added config file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 
+    'mongodb+srv://new-user-8762:aZooAOGcIv4yrMp3@postsapp.u0z7f.mongodb.net/mern-posts-app?retryWrites=true&w=majority',
+    SECRET_KEY: 'sgyjnFT67Ijf'
+}
